bbe4adbb95056ae84b4db011ed22e96d45468d16
Mario Adamo Jr <marioadamojr@gmail.com>
2019-07-17 16:05:26 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,9 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do #index
+    # binding.pry
+    @tweets = Tweet.all
+    erb :"/tweets/index"
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :"/users/signup"
+    else
+      redirect to "/tweets"
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.new(params)
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    # binding.pry
+    if !logged_in?
+      erb :"/users/login"
+    else
+      redirect "/tweets"
+    end
+  end
+
+  post '/login' do
+    redirect to "/tweets"
+  end
 
 end
